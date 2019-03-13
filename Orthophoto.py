18383e0c550b45ff23211635bd7a4c38f3d4b4e5
Sangwoo Ham <flyhamsw@gmail.com>
2019-03-14 01:00:05 +0900

    Revised Orthophoto.py : def rectify

--- Orthophoto.py
+++ Orthophoto.py
@@ -7,14 +7,19 @@ from EoData import readEO, convertCoordinateSystem, Rot3D
 from Boundary import boundary
 from BackprojectionResample import projectedCoord, backProjection, resample, createGeoTiff
 
-if __name__ == '__main__':
-    ground_height = 65  # unit: m
-    sensor_width = 6.3  # unit: mm
 
-    img_fname = 'DJI_0386.JPG'
-    eo_fname = 'DJI_0386.txt'
-    img_path = os.path.join('./Data', img_fname)
-    eo_path = os.path.join('./Data', eo_fname)
+def rectify(img_fname, eo_fname, project_path, ground_height, sensor_width):
+    """
+    In order to generate individual ortho-image, this function rectifies a given drone image on a reference plane.
+    :param img_fname:
+    :param eo_fname:
+    :param project_path:
+    :param ground_height: Ground height in m
+    :param sensor_width: Width of the sensor in mm
+    """
+
+    img_path = os.path.join(project_path, img_fname)
+    eo_path = os.path.join(project_path, eo_fname)
 
     start_time = time.time()
 
@@ -74,9 +79,19 @@ if __name__ == '__main__':
 
     print('Save the image in GeoTiff')
     start_time = time.time()
-    dst = './' + img_fname
+    dst = './' + img_fname.split('.')[0]
     createGeoTiff(b, g, r, a, bbox, gsd, boundary_rows, boundary_cols, dst)
     print("--- %s seconds ---" % (time.time() - start_time))
 
     print('*** Processing time per each image')
     print("--- %s seconds ---" % (time.time() - start_time + read_time))
+
+
+if __name__ == '__main__':
+    rectify(
+        img_fname='DJI_0386.JPG',
+        eo_fname='DJI_0386.txt',
+        project_path='./Data',
+        ground_height=0.65,
+        sensor_width=6.3
+    )
